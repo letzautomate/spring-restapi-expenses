f6d72124522b8d9e6790462d4fdffffa243231b3
ramakrishna <letzautomate@gmail.com>
2022-02-12 21:01:42 +0530

    initial commit

---
+++ src/test/java/com/letzautomate/restapi/springrestapiexpenses/SpringRestapiExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letzautomate.restapi.springrestapiexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestapiExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
